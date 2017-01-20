a162a12b036fb6f4e3082d1dd8c69a7502d356c0
numbergames <bfletch@gmail.com>
2017-01-20 12:16:19 -0500

    form submission

--- client/js/components/q-and-a.js
+++ client/js/components/q-and-a.js
@@ -7,23 +7,44 @@ class QandA extends Component {
   constructor(props) {
     super(props);
 
-    this.correctAnswer = this.correctAnswer.bind(this);
-    this.wrongAnswer = this.wrongAnswer.bind(this);
+    this.state = {
+      answer: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
     this.props.dispatch(actions.fetchQuestion(this.props.username));
   }
 
-  correctAnswer(e) {
-    this.props.dispatch(actions.postAnswer(true, this.props.username));
+  handleChange(event) {
+    this.setState({answer: event.target.value});
   }
 
-  wrongAnswer(e) {
-    this.props.dispatch(actions.postAnswer(false, this.props.username));
+  handleSubmit(event) {
+    let answer = this.props.question.question.answer.toLowerCase();
+    let submission = this.state.answer.toLowerCase();
+
+    console.log('Correct answer', answer);
+    console.log('Answer submitted', submission);
+
+    event.preventDefault();
+
+    if (answer === submission) {
+      alert('You got it right!');
+      this.props.dispatch(actions.postAnswer(true, this.props.username));
+    } else {
+      alert('Sorry, the correct answer was: ' + this.props.question.question.answer);
+      this.props.dispatch(actions.postAnswer(false, this.props.username));
+    }
+
+    this.setState({answer: ''});
   }
 
   render() {
+
     if (this.props.loading) {
       return (
         <div>
@@ -31,34 +52,40 @@ class QandA extends Component {
         </div>
       );
     }
+    console.log(this.props.question.question.answer);
 // submit form : "answer submit"
 // submit button: "answer button"
     return (
       <div>
-        <div className="tagalog card">
-          <h1 className="tagalog title">
+        <div className="tagalog-card">
+          <h1 className="tagalog-title">
             Tagalog:
           </h1>
-          <h2 className="tagalog question">
+          <h2 className="tagalog-question">
             {this.props.question.question.question}
           </h2>
         </div>
-        <div className="english card">
-          <h1 className="english title">
+        <div className="english-card">
+          <h1 className="english-title">
             English:
           </h1>
-          <h2 className="english question">
-            {this.props.question.question.answer}
-          </h2>
+          <form onSubmit={this.handleSubmit} className="answer-form">
+            <label>
+              Your answer:
+              <input
+                type="text"
+                className="answer-input"
+                value={this.state.answer}
+                onChange={this.handleChange}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </div>
-        <button id="right" onClick={this.correctAnswer}>
-          Correct
-        </button>
-        <button id="wrong" onClick={this.wrongAnswer}>
-          Incorrect
-        </button>
-        <br/>
-        <div className="score"> Current score:{this.props.question.score}</div>
+        <br />
+        <div className="score">
+          Current score:  {this.props.question.score}
+        </div>
       </div>
     );
   }
