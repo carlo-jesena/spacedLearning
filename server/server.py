9eeb71bcf78a1404bcad3e2d8ae1406231835715
Your Namecarlo <carlo.jesena@gmail.com>
2017-01-25 16:04:47 -0800

    added and connected to mongodb

--- server/server.py
+++ server/server.py
@@ -3,11 +3,21 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config['MONGOA_DBNAME'] = 'pythontest'
+app.config['MONGO_URI'] = 'mongodb://python:python@ds129469.mlab.com:29469/pythontest'
+
+mongo = PyMongo(app)
 
 
-@app.route("/hello")
-def say_hi():
-    return "Hello World!"
+@app.route('/add')
+def add():
+    user = mongo.db.users
+    user.insert({'name' : 'Chris'})
+    return 'Added User!'
+
+# @app.route("/hello")
+# def say_hi():
+#     return "Hello World!"
 
 
 
