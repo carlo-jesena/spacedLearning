bf2082cc39ac0ce87c22b9ea5f14232fa268ad4e
Your Namecarlo <carlo.jesena@gmail.com>
2017-01-25 23:13:11 -0800

    post route working with postman

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 // import express from 'express';
 // import mongoose from 'mongoose';
 // import bodyparser from 'body-parser';
-// import User from './models/user.js';
+// // import User from './models/user.js';
 //
 // mongoose.Promise = global.Promise;
 //
@@ -42,15 +42,15 @@
 //     score: 0,
 //     questions: [
 //       { question: "Salamat", answer: "Thank you", idx: 1, m: 1 },
-//       // { question: "Kamusta", answer: "How are you", idx: 2, m: 1},
-//       // { question: "Oo", answer: "Yes", idx: 3, m: 1 },
-//       // { question: "Hindi", answer: "No or Not", idx: 4, m: 1 },
-//       // { question: "Ako", answer: "I or Me", idx: 5, m: 1 },
-//       // { question: "Ikaw", answer: "You", idx: 6, m: 1 },
-//       // { question: "Sarap", answer: "Delicious", idx: 7, m: 1 },
-//       // { question: "Paumanhin", answer: "Sorry, excuse me", idx: 8, m: 1 },
-//       // { question: "Paalam", answer: "Farewell", idx: 9, m: 1 },
-//       // { question: "Tubig", answer: "Water", idx: 10, m: 1 }
+//       { question: "Kamusta", answer: "How are you", idx: 2, m: 1},
+//       { question: "Oo", answer: "Yes", idx: 3, m: 1 },
+//       { question: "Hindi", answer: "No or Not", idx: 4, m: 1 },
+//       { question: "Ako", answer: "I or Me", idx: 5, m: 1 },
+//       { question: "Ikaw", answer: "You", idx: 6, m: 1 },
+//       { question: "Sarap", answer: "Delicious", idx: 7, m: 1 },
+//       { question: "Paumanhin", answer: "Sorry, excuse me", idx: 8, m: 1 },
+//       { question: "Paalam", answer: "Farewell", idx: 9, m: 1 },
+//       { question: "Tubig", answer: "Water", idx: 10, m: 1 }
 //     ]
 //   })
 //   .then(
@@ -60,82 +60,82 @@
 //     res.status(500).json({message: 'Internal server error'})
 //   })
 // });
-//
-// // GET first question object and score of a logged in user
-// app.get('/users/:username', (req,res)=>{
-//     User.findOne(req.params)
-//     .then(userObj => {
-//       let initial = userObj;
-//       return res.status(201).json({score: initial.score, question: initial.questions[0]});
-//     })
-//     .catch(err => {
-//         res.status(500).json(err)
-//     })
-// })
-//
-// // GET userlist
-// app.get('/userlist', (req, res) => {
-//   User.find()
-//   .then(users => {
-//     let userlist = users.map(item => item.username);
-//   res.status(200).json(userlist)
-//   })
-//   .catch(err => res.status(500).json(err));
-// });
-//
-// // POST to retrieve next question
-// app.post('/answer/:username', (req,res) => {
-//   User.findOne(req.params)
-//   .then(userObj => {
-//         let score = userObj.score;
-//         if (req.body.answer === true) {
-//             score += 10;
-//         }
-//         let newQuestions = spacedRepAlgo(userObj.questions, req.body.answer);
-//           User.findOneAndUpdate(req.params, {$set:{score: score, questions: newQuestions}}, (user)=>{
-//           res.status(201).json({score: score, question: newQuestions[0]});
-//         });
-//       })
-//   .catch(err => {
-//     res.status(500).json(err)
-//     })
-// });
-//
-// app.post('/question/:username', (req, res) => {
-//   console.log(req.body);
-//
-//   const newQuestion = {
-//     question: req.body.question.question,
-//     answer: req.body.question.answer,
-//     m: 1
-//   };
-//   console.log(newQuestion);
-//
-//   User.findOneAndUpdate(
-//     req.params,
-//     { $push: { questions: newQuestion }},
-//     (user) => {
-//       res.sendStatus(201);
-//     }
-//   );
-//
-// });
-//
-// function runServer() {
-//     var databaseUri = process.env.DATABASE_URI || global.databaseUri || 'mongodb://carloben:carloben@ds111549.mlab.com:11549/spaced-learning';
-//     mongoose.connect(databaseUri)
-//     return new Promise((resolve, reject) => {
-//         app.listen(PORT, HOST, (err) => {
-//             if (err) {
-//                 console.error(err);
-//                 reject(err);
-//             }
-//             const host = HOST || 'localhost';
-//             console.log(`Listening on ${host}:${PORT}`);
-//         });
-//     });
-// }
-//
-// if (require.main === module) {
-//     runServer();
-// }
+
+// GET first question object and score of a logged in user
+app.get('/users/:username', (req,res)=>{
+    User.findOne(req.params)
+    .then(userObj => {
+      let initial = userObj;
+      return res.status(201).json({score: initial.score, question: initial.questions[0]});
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+// GET userlist
+app.get('/userlist', (req, res) => {
+  User.find()
+  .then(users => {
+    let userlist = users.map(item => item.username);
+  res.status(200).json(userlist)
+  })
+  .catch(err => res.status(500).json(err));
+});
+
+// POST to retrieve next question
+app.post('/answer/:username', (req,res) => {
+  User.findOne(req.params)
+  .then(userObj => {
+        let score = userObj.score;
+        if (req.body.answer === true) {
+            score += 10;
+        }
+        let newQuestions = spacedRepAlgo(userObj.questions, req.body.answer);
+          User.findOneAndUpdate(req.params, {$set:{score: score, questions: newQuestions}}, (user)=>{
+          res.status(201).json({score: score, question: newQuestions[0]});
+        });
+      })
+  .catch(err => {
+    res.status(500).json(err)
+    })
+});
+
+app.post('/question/:username', (req, res) => {
+  console.log(req.body);
+
+  const newQuestion = {
+    question: req.body.question.question,
+    answer: req.body.question.answer,
+    m: 1
+  };
+  console.log(newQuestion);
+
+  User.findOneAndUpdate(
+    req.params,
+    { $push: { questions: newQuestion }},
+    (user) => {
+      res.sendStatus(201);
+    }
+  );
+
+});
+
+function runServer() {
+    var databaseUri = process.env.DATABASE_URI || global.databaseUri || 'mongodb://carloben:carloben@ds111549.mlab.com:11549/spaced-learning';
+    mongoose.connect(databaseUri)
+    return new Promise((resolve, reject) => {
+        app.listen(PORT, HOST, (err) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            }
+            const host = HOST || 'localhost';
+            console.log(`Listening on ${host}:${PORT}`);
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer();
+}
--- server/server.py
+++ server/server.py
@@ -3,18 +3,50 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config['MONGOA_DBNAME'] = 'pythontest'
+app.config['MONGO_DBNAME'] = 'pythontest'
 app.config['MONGO_URI'] = 'mongodb://python:python@ds129469.mlab.com:29469/pythontest'
 
 mongo = PyMongo(app)
 
+## spaced repitition algorithm
+def spaced_rep_algo(arr, answer):
+    questions = arr.shift()
+    if answer == true:
+        questions['m'] *= 2
+    else:
+        questions['m'] = 1
+    arr.splice
 
-@app.route('/add')
-def add():
-    user = mongo.db.users
-    user.insert({'username' : 'Chris'})
-    return 'Added User!'
+# @app.route('/add')
+# def add():
+#     user = mongo.db.users
+#     user.insert({'username' : 'Chris'})
+#     return 'Added User!'
 
+### Add API endpoints here
+
+# POST new user--- creates a user object with username, score and questions properties
+@app.route('/users', methods=['POST'])
+def create_new_user():
+    users = mongo.db.users
+    username = request.json['username']
+    users.insert({
+        'username' : username,
+        'score' : 0,
+        'questions' : [
+            { 'question': "Salamat", 'answer': "Thank you", 'm': '1' },
+            { 'question': "Kamusta", 'answer': "How are you", 'm': '1'},
+            { 'question': "Oo", 'answer': "Yes", 'm': '1' },
+            { 'question': "Hindi", 'answer': "No or Not", 'm': '1' },
+            { 'question': "Ako", 'answer': "I or Me", 'm': '1' },
+            { 'question': "Ikaw", 'answer': "You", 'm': '1' },
+            { 'question': "Sarap", 'answer': "Delicious", 'm': '1' },
+            { 'question': "Paumanhin", 'answer': "Sorry, excuse me", 'm': '1' },
+            { 'question': "Paalam", 'answer': "Farewell", 'm': '1' },
+            { 'question': "Tubig", 'answer': "Water", 'm': '1' }]
+    })
+
+### GET users list
 @app.route('/userlist', methods=['GET'])
 def get_all_users():
     users = mongo.db.users
@@ -25,11 +57,6 @@ def get_all_users():
         userList.append({'username' : q['username']})
 
     return jsonify({'result' : userList})
-
-# @app.route("/hello")
-# def say_hi():
-#     return "Hello World!"
-
 
 
 ### running on localhost:8080 instead of cloud9
